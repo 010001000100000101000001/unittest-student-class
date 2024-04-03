8af16a909d24b9c399f62b4d8ad033f7b8ddc015
David Anderson <davidanderson@live.ie>
2024-04-03 14:27:48 +0000

    Third test

--- student.py
+++ student.py
@@ -16,5 +16,10 @@ class Student:
         return f"{self._first_name} {self._last_name}"
 
 
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+
     def alert_santa(self):
         self.naughty_list = True
